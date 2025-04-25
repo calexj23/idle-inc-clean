628717829b249dd219083e76b26afd67ce946281
calexj23 <calexjames@gmail.com>
2025-04-25 06:44:04 +0200

    Fix TypeScript style typing for pointerEvents

--- pages/index.tsx
+++ pages/index.tsx
@@ -193,7 +193,7 @@ const shareStyle = {
 };
 
 const floatingStyle = {
-  position: "absolute" as const,
+  position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
